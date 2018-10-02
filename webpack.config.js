4a4a3c9d20cafc6d0a8538e6b03ec85d72cbea5c
brahim <brahim.chaouchi@hotmail.fr>
2018-10-02 13:41:41 +0200

    active les css

--- webpack.config.js
+++ webpack.config.js
@@ -5,62 +5,85 @@ const UglifyJSPlugin = require("uglifyjs-webpack-plugin");
 const OptimizeCSSAssets = require("optimize-css-assets-webpack-plugin");
 const DashboardPlugin = require("webpack-dashboard/plugin");
 
-let config = {
-    entry: "./src/main.js",
-    output: {
-      path: path.resolve(__dirname, "./public"),
-      filename: "./bundle.js"
-    },
-    module: {
-        rules: [{
-          test: /\.js$/,
-          exclude: /node_modules/,
-          loader: "babel-loader"
-        },{
-		  test: /\.scss$/,
-		  loader: ['style-loader', 'css-loader', 'sass-loader', 'postcss-loader']
+let configjs = {
+	entry: "./src/main.js",
+	output: {
+		path: path.resolve(__dirname, "./public"),
+		filename: "./bundle.js"
+	},
+	module: {
+		rules: [{
+			test: /\.js$/,
+			exclude: /node_modules/,
+			loader: "babel-loader"
+		},{
+			test: /\.scss$/,
+			loader: ['style-loader', 'css-loader', 'sass-loader', 'postcss-loader']
 		},
-      {
-        test: /\.vue$/,
-        loader: 'vue-loader',
-        options: {
-          loaders: {
-            // Since sass-loader (weirdly) has SCSS as its default parse mode, we map
-            // the "scss" and "sass" values for the lang attribute to the right configs here.
-            // other preprocessors should work out of the box, no loader config like this necessary.
-            'scss': 'vue-style-loader!css-loader!sass-loader',
-            'sass': 'vue-style-loader!css-loader!sass-loader?indentedSyntax',
-          }
-          // other vue-loader options go here
-        }
-      }]
-      },
-  resolve: {
-    extensions: ['.ts', '.js', '.vue', '.json'],
-    alias: {
-      'vue$': 'vue/dist/vue.esm.js'
-    }
-  },
-      plugins: [
-		  //new ExtractTextWebpackPlugin("styles.css"),
-		  new UglifyJSPlugin(),
-		  new DashboardPlugin()
+		{
+			test: /\.vue$/,
+			loader: 'vue-loader',
+			options: {
+				loaders: {
+					// Since sass-loader (weirdly) has SCSS as its default parse mode, we map
+					// the "scss" and "sass" values for the lang attribute to the right configs here.
+					// other preprocessors should work out of the box, no loader config like this necessary.
+					'scss': 'vue-style-loader!css-loader!sass-loader',
+					'sass': 'vue-style-loader!css-loader!sass-loader?indentedSyntax',
+				}
+				// other vue-loader options go here
+			}
+		}]
+	},
+	resolve: {
+		extensions: ['.ts', '.js', '.vue', '.json'],
+		alias: {
+			'vue$': 'vue/dist/vue.esm.js'
+		}
+	},
+	plugins: [
+		//new ExtractTextWebpackPlugin("styles.css"),
+		new UglifyJSPlugin(),
+		new DashboardPlugin()
 		]
-  }
-  
-  module.exports = config;
-if (process.env.NODE_ENV === 'production') {
-  module.exports.plugins.push(
-    //new webpack.optimize.UglifyJsPlugin(),
-    new OptimizeCSSAssets(),
-    new webpack.LoaderOptionsPlugin({
-      minimize: true
-    })
-  );
-}else{
-  module.exports.plugins.push(
-    new webpack.LoaderOptionsPlugin({
-      minimize: false
-    })
-  );
 }
+let configcss = {
+	entry: "./assets/stylesheets/styles.scss",
+	output: {
+		path: path.resolve(__dirname, "./public"),
+		filename: "./style.js"
+	},
+	module: {
+		rules: [{
+			test: /\.js$/,
+			exclude: /node_modules/,
+			loader: "babel-loader"
+		},{
+			test: /\.scss$/,
+			loader: ['style-loader', 'css-loader', 'sass-loader', 'postcss-loader']
+		}]
+	},
+	plugins: [
+		//new ExtractTextWebpackPlugin("styles.css"),
+		new UglifyJSPlugin(),
+		new DashboardPlugin()
+	]
+}
+module.exports = [configjs, configcss];
+module.exports.forEach((exprt)=>{
+	if (process.env.NODE_ENV === 'production') {
+		exprt.plugins.push(
+			//new webpack.optimize.UglifyJsPlugin(),
+			new OptimizeCSSAssets(),
+			new webpack.LoaderOptionsPlugin({
+				minimize: true
+			})
+		);
+	}else{
+		exprt.plugins.push(
+			new webpack.LoaderOptionsPlugin({
+				minimize: false
+			})
+		);
+	}
+})
