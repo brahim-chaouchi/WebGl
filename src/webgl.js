7ff0a5f40c49d7d982f433564e36e591d8ed20c6
brahim <brahim.chaouchi@hotmail.fr>
2018-10-03 13:44:33 +0200

    exemple de triangle commenté

--- src/webgl.js
+++ src/webgl.js
@@ -1,92 +1,3 @@
-function render(gl,scene) {
-	gl.clear(gl.COLOR_BUFFER_BIT);
-	gl.useProgram(scene.program);
-	gl.bindBuffer(gl.ARRAY_BUFFER, scene.object.vertexBuffer);
-	gl.drawArrays(
-		scene.object.primitiveType, 0,
-		scene.object.vertexCount);
-	gl.bindBuffer(gl.ARRAY_BUFFER, null);
-	gl.useProgram(null);
-	requestAnimationFrame(function() {
-		render(gl,scene);
-	});
-}
-
-function createProgram(gl, shaderSpecs) {
-	var program = gl.createProgram();
-	for ( var i = 0 ; i < shaderSpecs.length ; i++ ) {
-	var spec = shaderSpecs[i];
-	var shader = gl.createShader(spec.type);
-	var source = document.getElementById(spec.container).text;
-	gl.shaderSource(shader, source);
-	gl.compileShader(shader);
-	if (!gl.getShaderParameter(shader, gl.COMPILE_STATUS)) {
-		throw gl.getShaderInfoLog(shader);
-	}
-	gl.attachShader(program, shader);
-	gl.deleteShader(shader);
-	}
-	gl.linkProgram(program);
-	if (!gl.getProgramParameter(program, gl.LINK_STATUS)) {
-	throw gl.getProgramInfoLog(program);
-	}
-	return program;
-}
-
-export function init2() {
-  var surface = document.getElementById('rendering-surface');
-  var gl = surface.getContext('webgl');
-  gl.viewport(0,0,surface.width,surface.height);
-  gl.clearColor(0.0, 0.0, 0.0, 1.0);
-
-  var program = createProgram(
-	gl,
-	[{container: 'vertex-shader', type: gl.VERTEX_SHADER},
-	 {container: 'fragment-shader', type: gl.FRAGMENT_SHADER}]
-  );
-
-  var squareVertices = [
-	+0.75, +0.75,
-	-0.75, +0.75,
-	+0.75, -0.75,
-	-0.75, -0.75
-  ];
-
-  gl.useProgram(program);
-
-  var square = {
-	vertexCount: 4,
-	primitiveType: gl.TRIANGLE_STRIP
-  };
-
-  var vertexBuffer = gl.createBuffer();
-  gl.bindBuffer(gl.ARRAY_BUFFER, vertexBuffer);
-
-  program.positionAttribute = gl.getAttribLocation(program, 'pos');
-  gl.enableVertexAttribArray(program.positionAttribute);
-  gl.vertexAttribPointer(
-	program.positionAttribute, 2, gl.FLOAT, false, 0, 0
-  );
-  gl.bufferData(
-	gl.ARRAY_BUFFER,
-	new Float32Array(squareVertices),
-	gl.STATIC_DRAW
-  );
-
-  gl.bindBuffer(gl.ARRAY_BUFFER, null);
-  gl.useProgram(null);
-	
-  square.vertexBuffer = vertexBuffer;
-
-  var scene = {
-	program: program,
-	object: square,
-  };
-
-  requestAnimationFrame(function() {
-	render(gl, scene);
-  });
-}
 export function init() {
 	//initialise le canvas
 	let surface = document.getElementById('rendering-surface');
@@ -99,7 +10,7 @@ export function init() {
 	gl.shaderSource(vertexShader, vertexText);
 	gl.compileShader(vertexShader);
 	if(!gl.getShaderParameter(vertexShader, gl.COMPILE_STATUS)){
-		throw "Erreur : Vertex compilation !";
+		throw "Erreur : Vertex compilation";
 	}
 	//initialise fragment shader
 	let fragmentText=document.getElementById('fragment-shader').text;
@@ -107,8 +18,60 @@ export function init() {
 	gl.shaderSource(fragmentShader, fragmentText);
 	gl.compileShader(fragmentShader);
 	if(!gl.getShaderParameter(fragmentShader, gl.COMPILE_STATUS)){
-		throw "Erreur : Fragment compilation !";
+		throw "Erreur : Fragment compilation";
 	}
+	//attache les shaders
+	let program=gl.createProgram();
+	gl.attachShader(program, vertexShader);
+	gl.attachShader(program, fragmentShader);
+	gl.linkProgram(program);
+	if(!gl.getProgramParameter(program, gl.LINK_STATUS)){
+		throw "Erreur : link";
+	}
+	gl.validateProgram(program);
+	if(!gl.getProgramParameter(program, gl.VALIDATE_STATUS)){
+		throw "Erreur : validation";
+	}
+	//cree les formes
+	//tableau de points
+	let formeBrut=[
+		//x, y, r, v, b
+		0.0, 0.5, 1.0, 0.0, 0.0,
+		-0.5,-0.5, 0.0, 1.0, 0.0,
+		0.5, -0.5, 0.0, 0.0, 1.0,
+	];
+	let formeBuffer=gl.createBuffer();
+	gl.bindBuffer(gl.ARRAY_BUFFER, formeBuffer);
+	gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(formeBrut), gl.STATIC_DRAW);
+	//attrib dans vertex-shader
+	//attribute pos
+	let attribLocation1=gl.getAttribLocation(program, 'pos');
+	gl.vertexAttribPointer(
+		attribLocation1,//attribute location
+		2,//elements per attribute
+		gl.FLOAT,//type
+		gl.FALSE,//is data normalised
+		5*Float32Array.BYTES_PER_ELEMENT,//size of individual vertex
+		0*Float32Array.BYTES_PER_ELEMENT //offset of attribute in vertex
+	);
+	gl.enableVertexAttribArray(attribLocation1);
+	//attribute vertColor
+	let attribLocation2=gl.getAttribLocation(program, 'vertColor');
+	gl.vertexAttribPointer(
+		attribLocation2,//attribute location
+		3,//elements per attribute
+		gl.FLOAT,//type
+		gl.FALSE,//is data normalised
+		5*Float32Array.BYTES_PER_ELEMENT,//size of individual vertex
+		2*Float32Array.BYTES_PER_ELEMENT //offset of attribute in vertex
+	);
+	gl.enableVertexAttribArray(attribLocation2);
 	//dessine
 	gl.clear(gl.COLOR_BUFFER_BIT, gl.DEPTH_BUFFER_BIT);
+	gl.useProgram(program);
+	gl.drawArrays(
+		gl.TRIANGLES,
+		0,//skip
+		3//nb to use
+	);
 }
